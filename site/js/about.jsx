46de6c57fcfe046f24518a12a6ae3dd75edd2dfe
Marianna Konarska - Czerwiakowska <marianna@MacBook-Pro-Marianna.local>
2018-05-15 16:11:07 +0200

    cv changed

--- site/js/about.jsx
+++ site/js/about.jsx
@@ -17,7 +17,7 @@ export default class About extends React.Component {
         return <div className='about' style={{left: this.props.isVisible ? "0" : "-100%"}}>
             <div className="close-btn pull-right" onClick={e => this.close() }/>
             <div className='containerAbout'>
-                <span>Nazywam się Marianna Konarska-Czerwiakowska i mieszkam w Warszawie. <br />  <br /> Jestem grafikiem i art directorem z ponad 10-letnim doświadczeniem w projektowaniu na potrzeby druku oraz digitalu.<br /> W 2017 postanowiłam zmienić swoją ścieżkę zawodową i zająć sie projektowaniem na potrzeby Front-endu. Po ukonczeniu stacjonarnego bootcampu organizowanego przez Coders Lab szukam stażu/pracy i zleceń jako Junior Front-end Developer.<br /> <br /> Będzie mi miło, jeżeli obejrzysz moje <a href="https://akbalmedia.blob.core.windows.net/public/mkonarska_cv.pdf"> CV</a> i skontaktujesz sie ze mną.</span>
+                <span>Nazywam się Marianna Konarska-Czerwiakowska i mieszkam w Warszawie. <br />  <br /> Jestem grafikiem i art directorem z ponad 10-letnim doświadczeniem w projektowaniu na potrzeby druku oraz digitalu.<br /> W 2017 postanowiłam zmienić swoją ścieżkę zawodową i zająć sie projektowaniem na potrzeby Front-endu. Po ukonczeniu stacjonarnego bootcampu organizowanego przez Coders Lab szukam stażu/pracy i zleceń jako Junior Front-end Developer.<br /> <br /> Będzie mi miło, jeżeli obejrzysz moje <a href="http://akbal.pl/cv_mkonarska.pdf"> CV</a> i skontaktujesz sie ze mną.</span>
             </div>
         </div>
     }
